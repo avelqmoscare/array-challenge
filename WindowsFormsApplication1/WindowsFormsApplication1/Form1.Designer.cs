fe6620337d1f482952a40db28ed9b621f14fc7db
avel moscare <avelqmoscare@gmail.com>
2018-09-04 10:49:23 +0800

    easy hahahha

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textLe = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(172, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textLe
+            // 
+            this.textLe.Location = new System.Drawing.Point(197, 48);
+            this.textLe.Name = "textLe";
+            this.textLe.Size = new System.Drawing.Size(75, 23);
+            this.textLe.TabIndex = 1;
+            this.textLe.Text = "button1";
+            this.textLe.UseVisualStyleBackColor = true;
+            this.textLe.Click += new System.EventHandler(this.textLe_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.textLe);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button textLe;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textLe_Click(object sender, EventArgs e)
+        {
+            String A = textBox1.Text;
+            string[,] MultiLetters = new string[5, 5]{
+                {A, "", "", "", "",},
+                {A, A, "", "", "",},
+                {A, A, A, "", "",},
+                {A, A, A, A, "",},
+                {A, A, A, A, A,},
+            };
+
+            string[] Letters = new string[5];
+            Letters[0] = MultiLetters[0, 0];
+            Letters[1] = MultiLetters[1, 0] + MultiLetters[1, 1];
+            Letters[2] = MultiLetters[2, 0] + MultiLetters[2, 1] + MultiLetters[2, 2];
+            Letters[3] = MultiLetters[3, 0] + MultiLetters[3, 1] + MultiLetters[3, 2] + MultiLetters[3, 3];
+            Letters[4] = MultiLetters[4, 0] + MultiLetters[4, 1] + MultiLetters[4, 2] + MultiLetters[4, 3] + MultiLetters[4, 4];
+
+
+            MessageBox.Show(Letters[0] + "\n" + Letters[1] + "\n" + Letters[2] + "\n" + Letters[3] + "\n" + Letters[4]);
+ 
+        }
+
+    }
+}
